18e58db061946fdf8564bd6b7fd0aef184a0684b
Adem Ayar <ademayar09@gmail.com>
2024-01-09 15:26:12 +0300

    screenshots added

--- app/src/main/java/com/aayar94/snake/core/theme/Theme.kt
+++ app/src/main/java/com/aayar94/snake/core/theme/Theme.kt
@@ -1,10 +1,14 @@
 package com.aayar94.snake.core.theme
 
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 
 
 private val LightColors = lightColorScheme(
@@ -75,16 +79,24 @@ private val DarkColors = darkColorScheme(
 @Composable
 fun SnakeTheme(
   useDarkTheme: Boolean = isSystemInDarkTheme(),
+  isDynamicColor: Boolean = true,
   content: @Composable() () -> Unit
 ) {
-  val colors = if (!useDarkTheme) {
-    LightColors
-  } else {
-    DarkColors
-  }
+
+    val dynamicColor = isDynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
+    val colorScheme = when {
+        dynamicColor && useDarkTheme -> {
+            dynamicDarkColorScheme(LocalContext.current)
+        }
+        dynamicColor && !useDarkTheme -> {
+            dynamicLightColorScheme(LocalContext.current)
+        }
+        useDarkTheme -> DarkColors
+        else -> LightColors
+    }
 
   MaterialTheme(
-    colorScheme = colors,
+    colorScheme = colorScheme,
     content = content
   )
 }
